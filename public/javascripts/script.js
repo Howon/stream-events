2918813a38ed114fb6f0f0055328f666ff81e35c
Howon Byun <howonbyun@gmail.com>
2015-03-27 13:33:48 -0400

    user identification & writing to json

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,18 @@
 var socket = io();
 
-$('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
-});
 
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+$(window).load(function() {
+	var person = prompt("Please enter your name");
+	if(person != null){
+		$('form').submit(function(){
+			input = [person, $('#m').val()];
+			socket.emit('chat message', input);
+			$('#m').val('');
+			return false;
+		});
+
+		socket.on('chat message', function(msg){
+			$('#messages').append($('<li>').text(person + ": " +msg));
+		});
+	}
 });
