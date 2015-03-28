d26049443c90780f7ae27f88ecc7f2ff7537d146
Howon Byun <howonbyun@gmail.com>
2015-03-28 03:48:01 -0400

    separated local testing and server side

--- app.js
+++ app.js
@@ -19,10 +19,26 @@ app.set('view engine', 'jade');
 app.use('/', routes);
 
 io.on('connection', function(socket){
-	
 	var CUSTOMCONNSTR_MONGOLAB_URI = 'mongodb://pioneer1625:95023680a@ds035617.mongolab.com:35617/stream-events';
 
-	mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+	
+	//-------------------------------------------------------------------------
+	//USE BELOW WHEN TESTING LOCALLY
+	//WHEN DONE TESTING COMMENT THIS OUT 
+    //  mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+    //      var collection = db.collection('chat_message_tester');
+    //      var stream = collection.find().sort({ _id : -1 }).limit(10).stream();
+    //      var stream = collection.find().sort().limit(10).stream();
+			// stream.on('data', function (chat) { 
+			// 	socket.emit('chat', chat.content); 
+			// 	console.log(chat.content);
+			// });
+    //  });
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+    //USE AFTER DONE TESTING LOCALLY	
+    mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
         var collection = db.collection('chat_message');
         var stream = collection.find().sort({ _id : -1 }).limit(10).stream();
         var stream = collection.find().sort().limit(10).stream();
@@ -31,7 +47,6 @@ io.on('connection', function(socket){
 				console.log(chat.content);
 			});
     });
-
 	socket.on('chat message', function(input){
 		var usr = input[0];
 		var msg = input[1];
@@ -41,15 +56,30 @@ io.on('connection', function(socket){
     		time: time,
     		content: msg
 		}
-		mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
-            var collection = db.collection('chat_messages');
-            collection.insert(data, function (err, o) {
-                if (err) { console.warn(err.message); }
-                else { console.log("chat message inserted into db: " + msg); }
-            });
-        });
-		io.emit('chat message', msg, usr);
 
+	//-------------------------------------------------------------------------
+	//USE BELOW WHEN TESTING LOCALLY
+	//WHEN DONE TESTING COMMENT THIS OUT 
+		// mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+  //           var collection = db.collection('chat_messages_tester');
+  //           collection.insert(data, function (err, o) {
+  //               if (err) { console.warn(err.message); }
+  //               else { console.log("chat message inserted into db: " + msg); }
+  //           });
+  //       });
+    //------------------------------------------------------------------------- 
+
+    //-------------------------------------------------------------------------
+	//USE AFTER DONE TESTING LOCALLY	
+	mongo.connect(CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+              var collection = db.collection('chat_messages');
+              collection.insert(data, function (err, o) {
+                  if (err) { console.warn(err.message); }
+                  else { console.log("chat message inserted into db: " + msg); }
+              });
+          });
+	//-------------------------------------------------------------------------   
+		io.emit('chat message', msg, usr);
 	});
 	socket.on('disconnect',function(){
 		console.log("User disconnected");
